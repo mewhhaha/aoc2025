9d065df6e5a6eb7e8a434fd155a604774537272d
mewhhaha <kopatheonlyone@hotmail.com>
2025-12-01 19:39:38 +0100

    clean up

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -1,50 +1,42 @@
 module Main (main) where
 
-import Control.Exception (assert)
-import Debug.Trace (trace)
 
-data Rotation = L Int | R Int
-
-expect a b = do
-  if a == b then return () else error ("Expected " ++ show a ++ " to equal " ++ show b)
+expect a b
+  | a == b = pure ()
+  | otherwise = error ("Expected " ++ show a ++ " to equal " ++ show b)
 
 main :: IO ()
 main = do
   input <- readFile "./input.txt"
   test <- readFile "./test1.txt"
   expect (part1 test) 3
-  print $ part1 input
+  print (part1 input)
   expect (part2 test) 6
-  print $ part2 input
+  print (part2 input)
 
 part1 :: String -> Int
 part1 file =
   let content = map parse . lines $ file
-      rotations = scanl apply 50 content
+      rotations = scanl step 50 content
       zeros = length (filter (== 0) rotations)
    in zeros
 
-apply r (L n) = (r - n) `mod` 100
-apply r (R n) = (r + n) `mod` 100
+step r move = (r + move) `mod` 100
 
-parse ('L' : rest) = L (read rest)
-parse ('R' : rest) = R (read rest)
-parse unknown = error unknown
+parse ('L' : rest) = negate (read rest)
+parse ('R' : rest) = read rest
 
 part2 :: String -> Int
 part2 file =
   let content = map parse . lines $ file
-      rotations = scanl apply2 (0, 50) content
+      rotations = scanl advance (0, 50) content
       clicks = sum $ map fst rotations
    in clicks
 
-apply2 (_, r) (L n) =
-  let rest = n `rem` 100
-      during = n `div` 100
-      end = if r /= 0 && r - rest <= 0 then 1 else 0
-   in (during + end, (r - rest) `mod` 100)
-apply2 (_, r) (R n) =
-  let rest = n `rem` 100
-      during = n `div` 100
-      end = if r /= 0 && r + rest >= 100 then 1 else 0
-   in (during + end, (r + rest) `mod` 100)
+advance (_, r) move =
+  let (loops, rest) = abs move `divMod` 100
+      end
+        | r == 0 = False
+        | move < 0 = r - rest <= 0
+        | otherwise = r + rest >= 100
+   in (loops + fromEnum end, (r + move) `mod` 100)
