ec6dcb7b2a93a94fe2d06df8defcb6d9642eaa94
Jacob Torrång <kopatheonlyone@hotmail.com>
2025-12-09 13:15:48 +0100

    not working

--- day9/app/Main.hs
+++ day9/app/Main.hs
@@ -1,8 +1,9 @@
 module Main (main) where
 
+import Data.Either (partitionEithers)
 import Data.Function (on)
 import Data.List (elemIndex, findIndex, nub, sort, sortBy, tails)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, listToMaybe)
 import Data.Ord
 import Debug.Trace (trace)
 
@@ -30,46 +31,57 @@ parse txt = map parseTuple $ lines txt
         (Just n) -> (read $ take n row, read $ drop (n + 1) row)
         Nothing -> error "expected coord"
 
-size ((x1,y1),(x2,y2)) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
-
+size ((x1, y1), (x2, y2)) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
 
 part1 txt =
   let coords = parse txt
-      pairs = [(a,b) | (a:rest) <- tails coords, b <- rest]
-  in case sortBy (comparing Down) $ map size pairs of
-        (x:_) -> x
-        _ -> error "empty list"
+      pairs = [(a, b) | (a : rest) <- tails coords, b <- rest]
+   in case listToMaybe $ sortBy (comparing Down) $ map size pairs of
+        Just x -> x
+        Nothing -> error "empty list"
 
-contains ((x1,y1),(x2, y2)) (a, b) = let left = min x1 x2
-                                         right = max x1 x2
-                                         top = min y1 y2
-                                         bottom = max y1 y2
-                                      in a > left && a < right && b > top && b < bottom
+contains ((x1, y1), (x2, y2)) (a, b) =
+  let left = min x1 x2
+      right = max x1 x2
+      top = min y1 y2
+      bottom = max y1 y2
+   in a > left && a < right && b > top && b < bottom
 
-corners ((x1,y1), (x2, y2)) =
-    let left = min x1 x2
-        right = max x1 x2
-        top = min y1 y2
-        bottom = max y1 y2
-    in ((left, top), (right, top), (left, bottom), (left, top))            
-isPartOf :: [((Int, Int), (Int, Int), (Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))] -> Bool
-isPartOf [] rects = True
-isPartOf _ [] = False
-isPartOf test rects = isPartOf (concatMap go test) rects
+overlaps ((x1, y1), (x2, y2)) (a, b) =
+  let left = min x1 x2
+      right = max x1 x2
+      top = min y1 y2
+      bottom = max y1 y2
+   in a >= left && a <= right && b >= top && b <= bottom
 
-          where
-             go all@(topLeft, topRight, bottomLeft, bottomRight) =
-                      let checks = foldl' (\(a,b,c,d) rect -> (a || rect `contains` topLeft, b || rect `contains` topRight, c || rect `contains` bottomLeft, d || rect `contains` bottomRight ) ) (False, False, False, False) rects
-                      in case checks of
-                                (False, False, False, False ) -> [all]
-                                (True, True, True, True) -> []
+corners ((x1, y1), (x2, y2)) =
+  let left = min x1 x2
+      right = max x1 x2
+      top = min y1 y2
+      bottom = max y1 y2
+   in [(left, top), (right, top), (left, bottom), (right, bottom)]
 
-
+process :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
+process = go []
+  where
+    go valid remaining =
+      let (left, right) =
+            partitionEithers $
+              map
+                ( \pair -> case pair of
+                    ((x1, y1), (x2, y2))
+                      | x1 == x2 || y1 == y2 -> Right pair
+                      | all (\corner -> any (`overlaps` corner) valid) (corners pair) -> Right pair
+                      | otherwise -> Left pair
+                )
+                remaining
+       in case right of
+            [] -> valid
+            _ -> go (valid ++ right) left
 
 part2 txt =
   let coords = parse txt
-      pairs = [(a,b) | (a:rest) <- tails coords, b <- rest]
-      rects = [rect| rect <- pairs , not $ any (rect `contains`) coords]
-  in case sortBy (comparing Down) $ map size $ filter (\x -> [corners x] `isPartOf` rects) pairs of
-      (x:_) -> x
-      _ -> error "empty list"
+      pairs = [(a, b) | (a : rest) <- tails coords, b <- rest]
+   in case listToMaybe $ sortBy (comparing Down) $ map size $ process pairs of
+        Just x -> x
+        Nothing -> error "empty list"
